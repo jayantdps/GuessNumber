62340dbe0289d615763f8c9100dbc64cbde8a858
Jayant Kaushik <jayantdps@gmail.com>
2022-07-12 08:49:38 +0530

    mvp 2

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ console.log(
 
     */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 // state variable
 let score = 20; 
@@ -68,4 +68,19 @@ if (score > 0){
     }
     
   }
+});
+
+// resetting the game
+document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
+   secretNumber = Math.trunc(Math.random() * 20) + 1;
+   document.querySelector('.message').textContent = 'Start guessing...';
+   document.querySelector('.score').textContent = score;
+   document.querySelector('.number').textContent = '?';
+   document.querySelector('.guess').value = '';
+   
+   document.querySelector('body').style.backgroundColor = '#222';
+
+   document.querySelector('.number').style.width = '15rem';
+
 });
